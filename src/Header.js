111620fe44efb184619b2a86310adf3a4923170f
Ervin156 <dmitriylystov@gmail.com>
2019-08-29 19:16:37 +0300

    v1

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Menu from "./Menu";
+import Cart from "./Cart";
+function Header() {
+  return (
+    <div>
+      <Menu />
+      <Cart />
+    </div>
+  );
+}
+export default Header;
